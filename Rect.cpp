3bc44523f497bc19ec4d6f0998ebd9bd25189115
Bm07-cmd <bmai24598@gmail.com>
2025-06-28 10:53:55 -0700

    Update Rect.cpp

--- Rect.cpp
+++ Rect.cpp
@@ -14,6 +14,16 @@ Rect::Rect(int x, int y, int w, int h){
 
 
 void Rect::draw(SDL_Renderer*& renderer){
-	SDL_SetRenderDrawColor(renderer, 255, 0,0,0);
-	SDL_RenderDrawRect(renderer, &pos_rect);
+	SDL_SetRenderDrawColor(renderer, 0, 0,255,0);
+	//SDL_RenderDrawRect(renderer, &pos_rect);
+	SDL_RenderFillRect(renderer, &pos_rect);
+}
+
+void Rect::update(int WINDOW_HEIGHT){
+	y_pos += 6;
+	if(y_pos > WINDOW_HEIGHT){
+		y_pos = -height;
+	}
+	pos_rect.x = x_pos;
+	pos_rect.y = y_pos;
 }
